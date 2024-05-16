e703773e8aa77de340c87fb43188cb435957434d
HayahMahmoud <168680717+HayahMahmoud@users.noreply.github.com>
2024-05-16 23:34:21 +0300

    Create get_distance.h

---
+++ Second Milestone/get_distance.h
@@ -0,0 +1,9 @@
+#include "tm4c123gh6pm.h"
+#include <string.h>
+#include <stdint.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+float get_distance(float current_long,float current_lat,float next_long,float next_lat);
