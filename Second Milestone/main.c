02af3cd56541562457757b430c99e36e71d3391a
OmarAboBakr7 <89665983+OmarAboBakr7@users.noreply.github.com>
2024-05-16 22:55:42 +0300

    Update main.c

--- Second Milestone/main.c
+++ Second Milestone/main.c
@@ -64,24 +64,23 @@ int main(){
     long_1();
 
     while(1){
-    UART7_readstr(test);
+        UART7_readstr(test);
 
-    splitString(test,',',splited);
+        splitString(test,',',splited);
 
-    lat_2();
+        lat_2();
         long_2();
         //sprintf(test2, "%.*f", 5, distance);
         //UART0_printf(test2);
         //UART0_printf("\n");
         if(get_distance(next_long,next_lat,current_long,current_lat)>3&&get_distance(next_long,next_lat,current_long,current_lat)<150){
-            lat =(float)atof(splited[3])*100000;
-                  EEPROM_Write(counter_eeprom_write,lat);
-                  counter_eeprom_write+=4;
-                  loong =(float)atof(splited[5])*100000;
-                  EEPROM_Write(counter_eeprom_write,loong);
-                  counter_eeprom_write+=4;
-        distance=distance+get_distance(next_long,next_lat,current_long,current_lat);
-
+            lat = (float)atof(splited[3])*100000;
+            EEPROM_Write(counter_eeprom_write,lat);
+            counter_eeprom_write+=4;
+            loong = (float)atof(splited[5])*100000;
+            EEPROM_Write(counter_eeprom_write,loong);
+            counter_eeprom_write+=4;
+            distance=distance+get_distance(next_long,next_lat,current_long,current_lat);
             delay_IN_ms(500);
             sprintf(test2, "%.*f", 5, distance);
             LCD_CMD(0x80);
@@ -92,24 +91,23 @@ int main(){
             long_1();
             if(distance>100){
                 LCD_CMD(0x80);
-                           LCD_SendString("DIS:",4);
-                           LCD_CMD(0xC0);
-                           LCD_SendString("Arrived  ",9);
+                LCD_SendString("DIS:",4);
+                LCD_CMD(0xC0);
+                LCD_SendString("Arrived  ",9);
                 GPIO_PORTF_DATA_R=0x04;
-                       for( counter_eeprom_read=0;counter_eeprom_read <counter_eeprom_write ; counter_eeprom_read+=8){
-                       get_deg(EEPROM_Read(counter_eeprom_read),EEPROM_Read(counter_eeprom_read+4));
-
-
-                   }
-                       GPIO_PORTF_DATA_R=0x08;
-            while(1){
-
-                UART0_printf(lattt);
+                
+                for(counter_eeprom_read=0;counter_eeprom_read <counter_eeprom_write ; counter_eeprom_read+=8){
+                    get_deg(EEPROM_Read(counter_eeprom_read),EEPROM_Read(counter_eeprom_read+4));
+                }
+                GPIO_PORTF_DATA_R=0x08;
+                while(1){
+                    UART0_printf(lattt);
                     UART0_printf(",");
                     UART0_printf(longgg);
                     UART0_printf("\n");
-
-            }}
+                }
+            }
+        }
     }
-    }}
+}
 
